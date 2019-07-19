38fcc33afbd15560fa51b58faa4798b918be0b0c
santi2019 <santiagovietto5@hotmail.com>
2019-07-19 12:39:28 -0300

    Terminado

--- misFunciones.js
+++ misFunciones.js
@@ -26,7 +26,11 @@ function fuerza() {
     console.log(document.Camp.carga1.value);
     console.log(document.Camp.carga2.value);
     console.log(document.Camp.distancia.value);
-    document.Camp.fuerzat.value = ( 9 * Math.pow(10, 9) * Number(document.Camp.carga1.value) * Number(document.Camp.carga2.value) ) / ( Number(document.Camp.distancia.value) * Number(document.Camp.distancia.value) );
+
+    if (Camp.carga1.value !="" && Camp.carga2.value != "" && Camp.distancia.value !="" ) { // pregunto si los campos no estan vacios
+
+        document.Camp.fuerzat.value = ( 9 * Math.pow(10, 9) * Number(document.Camp.carga1.value) * Number(document.Camp.carga2.value) ) / ( Number(document.Camp.distancia.value) * Number(document.Camp.distancia.value) );
+    }
 }
 
 
